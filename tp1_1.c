b56519f09dda961a8759c4d3d7b6d97e5b83c875
Gonzalo Robinson <gonchyrobinson@gmail.com>
2023-03-17 11:24:52 -0300

    Agrego puntero

--- tp1_1.c
+++ tp1_1.c
@@ -1,6 +1,12 @@
 # include <stdio.h>
 int main(){
     printf("Hola mundo");
-    printf("Correcto");
+    int x = 7;
+    int * px = &x;
+    printf("\nContenido del puntero(%d)", (*px));
+    printf("\ndireccion de memoria almacenada por el punter %p", &x);
+    printf("\nDireccion de memoria puntero: %p", &px);
+    printf("\nTamanio de memoria utlizado por esa variable: %i",sizeof(px));
+    printf("\nDireccion de memoria almacenada por el puntero %p", px);
     return(0);
 }
